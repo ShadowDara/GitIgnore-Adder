ab25e5d3b067105bb3b9002fe91a505187b46d62
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-02-18 13:46:12 +0100

    working now

---
+++ build.bat
@@ -0,0 +1,3 @@
+echo Building Gitignore Adder with Zig
+
+zig cc -Os main.c oscore.c -o main.exe
--- main.c
+++ main.c
@@ -7,15 +7,16 @@
 
 #include "oscore.h"
 
-
 // Check if a File exists
-int file_exists(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file) {
-        fclose(file);
-        return 1;
-    }
-    return 0;
+int file_exists(const char *filename)
+{
+	FILE *file = fopen(filename, "r");
+	if (file)
+	{
+		fclose(file);
+		return 1;
+	}
+	return 0;
 }
 
 // Screaming for help
@@ -24,34 +25,52 @@ void help()
 	printf("HELP!\n");
 }
 
-
 // Main Function
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	// TODO
 	// Pipeline
 	//
 	// Get the Current Path
-	//char Path* = get_current_path();
+	// char Path* = get_current_path();
 
-	const char* folder_to_check = ".git";
+	const char *folder_to_check = ".git";
 
 	int GitFolderFound = 1;
 	int GitignoreFound = 1;
 
 	// Check if there is a Git Repository in the same Directory
-	if (folder_exists_in_current(folder_to_check)) {
-	        printf("Folder '%s' exists in the current directory!\n", folder_to_check);
+	if (folder_exists_in_current(folder_to_check))
+	{
+		printf("Folder '%s' exists in the current directory!\n", folder_to_check);
 		GitFolderFound = 0;
-    	} else {
-        	printf("Folder '%s' does NOT exist in the current directory.\n", folder_to_check);
-    	}
+	}
+	else
+	{
+		printf("Folder '%s' does NOT exist in the current directory.\n", folder_to_check);
+	}
 
 	if (GitFolderFound == 0)
 	{
 		if (file_exists(".gitignore") == 1)
 		{
 			printf("Gitignore Found\n");
+			FILE *file = fopen(".gitignore", "a"); // "a" = append
+			if (file == NULL)
+			{
+				perror("Fehler beim Öffnen der Datei");
+				return 1;
+			}
+			for (int i = 1; i < argc; i++)
+			{
+				printf("argv[%d] = %s\n", i, argv[i]);
+				// Zeile anhängen
+				fprintf(file, "%s", argv[i]);
+				fprintf(file, "\n");
+			}
+
+			// Close the File
+			fclose(file);
 		}
 	}
 
@@ -66,9 +85,6 @@ int main(int argc, char* argv[])
 	// append every argument as a new Line to the gitignore
 	// file
 
-
-
 	// printf("Hello World\n");
 	return 0;
 }
-
